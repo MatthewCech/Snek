b31a2139d8c5722cfc878be6268b45ae4ae5c679
Matthew Cech <contact@matthewcech.com>
2020-08-01 11:46:31 -0700

    Added boilerplate and whatnot

---
+++ Snek/Snek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Snek
+{
+    public class SnekLauncher
+    {
+        public static Snek Instance = null;
+        static void Main(string[] args)
+        {
+            Instance = new Snek();
+            Instance.Run().GetAwaiter().GetResult();
+        }
+    }
+
+    public class Snek
+    {
+        private DiscordSocketClient client;
+        public async Task Run()
+        {
+            client = new DiscordSocketClient();
+            await client.LoginAsync(TokenType.Bot, "TOKEN HERE");
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
