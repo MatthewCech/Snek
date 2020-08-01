20c31b6db30f10d4f267ed82bb8f5252f98419b7
Matthew Cech <contact@matthewcech.com>
2020-08-01 12:50:23 -0700

    Added ini parser and some ping style boilerplate

--- Snek/Snek/Program.cs
+++ Snek/Snek/Program.cs
@@ -2,28 +2,80 @@
 using Discord;
 using Discord.WebSocket;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using Snek.Utils;
+using System.Collections.Generic;
 
 namespace Snek
 {
-    public class SnekLauncher
+    public interface ISnek
     {
-        public static Snek Instance = null;
+        string Name { get; }
+    }
+
+    public class SnekBasket
+    {
+        public static List<ISnek> Basket = null;
+
         static void Main(string[] args)
         {
-            Instance = new Snek();
-            Instance.Run().GetAwaiter().GetResult();
+            Basket = new List<ISnek>();
+
+            GarterSnek snek = new GarterSnek(configPath: "./garter.snek");
+            snek.Slither().GetAwaiter().GetResult();
+            Basket.Add(snek);
         }
     }
 
-    public class Snek
+    public class GarterSnek : ISnek
     {
         private DiscordSocketClient client;
-        public async Task Run()
+
+        public string Name { get; private set; }
+
+        private string token;
+        private string indicator;
+
+        public GarterSnek(string configPath)
+        {
+            PseudoINI garterConfig = new PseudoINI(configPath);
+            this.Name = garterConfig.ReadItem("name");
+            this.token = garterConfig.ReadItem("token");
+            this.indicator = garterConfig.ReadItem("indicator");
+        }
+
+        // Snakes can't run, but they can slither
+        public async Task Slither()
         {
             client = new DiscordSocketClient();
-            await client.LoginAsync(TokenType.Bot, "TOKEN HERE");
+            client.MessageReceived += Client_MessageReceived;
+
+            await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
+
+            
             await Task.Delay(-1);
+        }
+
+        private Task Client_MessageReceived(SocketMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage raw)
+        {
+            // Ignore self
+            if (raw.Author.Id == client.CurrentUser.Id)
+                return;
+
+            string message = raw.Content.Trim();
+            if(message != null && message.Length > 0)
+            {
+                if(message.StartsWith(indicator))
+                {
+                    await raw.Channel.SendMessageAsync("*hiss*");
+                }
+            }
         }
     }
 }
---
+++ Snek/Snek/Utils/INIParser.cs
@@ -0,0 +1,129 @@
+﻿using System.Xml;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Net.NetworkInformation;
+using Discord;
+using System.Text.Json;
+
+namespace Snek.Utils
+{
+    // Quick and dirty INI read/write WITHOUT LABELS for configs and whatnot. Not to INI spec
+    // This works as an INI file in other parsers, but other parsers may not output in this format.
+    // - Not thread safe, but won't burn the whole program to the floor if it fails.
+    // - Keys are case insensitive, valeus are not.
+    // - No multiline
+    public class PseudoINI
+    {
+        // Properties
+        public string Path { get; private set; }
+        public bool Exists => File.Exists(Path);
+
+        private string[] reserved = { "=", ";", "\n" };
+        
+        public PseudoINI(string path)
+        {
+            Path = path;
+        }
+
+        // Sets an item if it's present, add if it's not.
+        public bool WriteItem(string key, string value)
+        {
+            // Invalid characters not permitted.
+            foreach (string s in reserved)
+            {
+                if (key.Contains(s))
+                    return false;
+            }
+
+            try
+            {
+                List<KeyValuePair<string, string>> vals = Snapshot();
+
+                // Case: no file / null
+                if (vals == null)
+                {
+                    vals = new List<KeyValuePair<string, string>>();
+                    vals.Add(new KeyValuePair<string, string>(key, value));
+                }
+                else
+                {
+                    bool found = false;
+                    for (int i = 0; i < vals.Count; ++i)
+                    {
+                        // Case: Existing key
+                        if (KeyEquals(vals[i].Key, key))
+                        {
+                            vals[i] = new KeyValuePair<string, string>(key, value);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    // Case: key doesn't exist but file does
+                    if (!found)
+                    {
+                        vals.Add(new KeyValuePair<string, string>(key, value));
+                    }
+                }
+
+                string toWrite = Compose(vals);
+                File.WriteAllText(Path, toWrite);
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public string ReadItem(string key)
+        {
+            List<KeyValuePair<string, string>> snap = Snapshot();
+            foreach(KeyValuePair<string, string> pair in snap)
+            {
+                if (KeyEquals(pair.Key, key))
+                    return pair.Value;
+            }
+
+            return null;
+        }
+
+        private bool KeyEquals(string k1, string k2)
+        {
+            return k1.Trim().ToLower().Equals(k2.Trim().ToLower());
+        }
+
+        // Snapshot reading style - the file isn't kept open. This isn't particularly efficient.
+        // May throw an error if it explodes.
+        private List<KeyValuePair<string, string>> Snapshot()
+        {
+            if (!Exists)
+                return null;
+
+            List<KeyValuePair<string, string>> parsed = new List<KeyValuePair<string, string>>();
+            string text = File.ReadAllText(Path).Trim();
+            string[] split = text.Split('\n');
+                
+            foreach(string s in split)
+            {
+                string[] line = s.Trim().Split('=');
+                parsed.Add(new KeyValuePair<string, string>(line[0], line[1]));
+            }
+
+            return parsed;
+        }
+
+        private string Compose(List<KeyValuePair<string, string>> toCompose)
+        {
+            string built = "";
+            foreach (KeyValuePair<string, string> pair in toCompose)
+            {
+                built += pair.Key + "=" + pair.Value + "\n";
+            }
+
+            return built;
+        }
+    }
+}
